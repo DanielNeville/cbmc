d546ce03df5c0095a99c52f1b82374893bc19e26
Daniel Neville <daniel.neville@cs.ox.ac.uk>
2016-09-29 13:22:05 +0100

    Parse options, path search, stricter regression test.

--- regression/symex-taint-analysis/loop-taint-inline/main.c
+++ regression/symex-taint-analysis/loop-taint-inline/main.c
@@ -12,12 +12,15 @@ int main() {
 
 	for (i = 0; i < 4; i++) {
 
-		if (i == 4)
+		if (i == 3)
 			x[i] = j;
 		else
 			x[i] = 0;
 	}
 
+	assert(__CPROVER_is_taint("main::1::x[0]", "untainted"));
+	assert(__CPROVER_is_taint("main::1::x[1]", "untainted"));
+	assert(__CPROVER_is_taint("main::1::x[2]", "untainted"));
 	assert(__CPROVER_is_taint("main::1::x[3]", "tainted"));
 
 	return 0;
